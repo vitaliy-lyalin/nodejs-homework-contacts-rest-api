7c3d9ed768d7695346638c44fd12f384a6a5f275
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-07-04 15:11:43 +0300

    Adds Mongoose Schema and model

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,12 +10,12 @@ const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+// router.get("/:contactId", ctrl.getById);
 
-router.post("/", validateBody(addSchema), ctrl.add);
+// router.post("/", validateBody(addSchema), ctrl.add);
 
-router.delete("/:contactId", ctrl.deleteById);
+// router.delete("/:contactId", ctrl.deleteById);
 
-router.put("/:contactId", validateBody(addSchema), ctrl.updateById);
+// router.put("/:contactId", validateBody(addSchema), ctrl.updateById);
 
 module.exports = router;
